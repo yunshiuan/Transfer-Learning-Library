f803e070dca721182a0a2ff5be613efe24776b77
yunshiuan_lando <yunshiuan.chuang@gmail.com>
2022-04-22 00:26:27 +0000

    add TODO

--- sandbox/dann.py
+++ sandbox/dann.py
@@ -373,8 +373,8 @@ if __name__ == '__main__':
     # - https://tl.thuml.ai/get_started/quickstart.html
     # - CUDA_VISIBLE_DEVICES=0 python dann.py data/office31 -d Office31 -s A -t W -a resnet50 --epochs 20 --seed 1 --log logs/dann/Office31_A2W
     # - par
-    DATASET = "Office31"
-    # DATASET = "Office31_v2"
+    # DATASET = "Office31"
+    DATASET = "Office31_v2"
     VERSION = "v3"
     PHASE = 'train'
     # PHASE = 'test'
